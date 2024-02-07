5e74c4471cd49eddb65c2d805a19e8cf7c9d7e14
Farras Timorremboko <farras.timorremboko@corp.bri.co.id>
2024-02-07 10:35:27 +0700

    make api about car

---
+++ routers/carRouter.go
@@ -0,0 +1,23 @@
+package routers
+
+import (
+	"belajar-gin/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func StartServer() *gin.Engine {
+	router := gin.Default()
+
+	router.POST("/cars", controllers.CreateCar)
+
+	router.PUT("/cars/:carId", controllers.UpdateCar)
+
+	router.GET("/cars/:carId", controllers.GetCar)
+
+	router.GET("/cars", controllers.GetAllCar)
+
+	router.DELETE("/cars/:carId", controllers.DeleteCar)
+
+	return router
+}
